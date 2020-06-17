084308d755302968983f24392b541d69cad44309
mina gerges <mina.gerges@isae.edu.lb>
2020-06-17 14:18:31 +0300

    spring boot

---
+++ src/main/java/com/lov2codce/springdemo/com/mycoolapp/rest/FunRestController.java
@@ -0,0 +1,27 @@
+package com.lov2codce.springdemo.com.mycoolapp.rest;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+@RestController
+public class FunRestController {
+
+    @Value("${coach.name}")
+    private String coachName;
+
+    //expose "/" that return "Hello World"
+    @GetMapping("/")
+    public String sayHello(){
+        return "Hello World! Coach Name is:  "  + coachName;
+    }
+
+    //EXPOSE A NEW ENDPOINT FOR "WORKOUT"
+    @GetMapping("/workout")
+    public String getDailyWorkout(){
+        return "Run a hard 5k!";
+    }
+}
